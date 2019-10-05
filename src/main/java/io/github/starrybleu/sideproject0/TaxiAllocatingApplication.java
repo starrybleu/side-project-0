77a916dfab3e568797664d94ea26f55afffcdecf
Hanbyul Lee <starrybleu@gmail.com>
2019-10-05 23:27:17 +0900

    테스트용 데이터 + todos

--- src/main/java/io/github/starrybleu/sideproject0/TaxiAllocatingApplication.java
+++ src/main/java/io/github/starrybleu/sideproject0/TaxiAllocatingApplication.java
@@ -3,6 +3,11 @@ package io.github.starrybleu.sideproject0;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// todo - [ ] swagger `@ApiOperation`
+// todo - [ ] 테이블 설계 문서
+// todo - [ ] readme.md 추가
+// todo - [ ] 통합 테스트 코드
+
 @SpringBootApplication
 public class TaxiAllocatingApplication {
 
