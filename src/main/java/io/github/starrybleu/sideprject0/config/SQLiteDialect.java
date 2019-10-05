f1b68529f398e1ee6b9b0266409fcbafb20f8e01
Hanbyul Lee <starrybleu@gmail.com>
2019-10-05 15:19:05 +0900

    Configure SQLite settings.

---
+++ src/main/java/io/github/starrybleu/sideprject0/config/SQLiteDialect.java
@@ -0,0 +1,50 @@
+package io.github.starrybleu.sideprject0.config;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.identity.IdentityColumnSupport;
+
+import java.sql.Types;
+
+public class SQLiteDialect extends Dialect {
+
+    public SQLiteDialect() {
+        registerColumnType(Types.BIT, "integer");
+        registerColumnType(Types.TINYINT, "tinyint");
+        registerColumnType(Types.SMALLINT, "smallint");
+        registerColumnType(Types.INTEGER, "integer");
+        registerColumnType(Types.BIGINT, "long");
+    }
+
+    @Override
+    public IdentityColumnSupport getIdentityColumnSupport() {
+        return new SQLiteIdentityColumnSupport();
+    }
+
+    // note SQLite 가 db 제약조건(constraints)를 지원하지 않기 때문에 명시적으로 disable 처리
+    @Override
+    public boolean hasAlterTable() {
+        return false;
+    }
+
+    @Override
+    public boolean dropConstraints() {
+        return false;
+    }
+
+    @Override
+    public String getDropForeignKeyString() {
+        return "";
+    }
+
+    @Override
+    public String getAddForeignKeyConstraintString(String cn,
+                                                   String[] fk, String t, String[] pk, boolean rpk) {
+        return "";
+    }
+
+    @Override
+    public String getAddPrimaryKeyConstraintString(String constraintName) {
+        return "";
+    }
+
+}
