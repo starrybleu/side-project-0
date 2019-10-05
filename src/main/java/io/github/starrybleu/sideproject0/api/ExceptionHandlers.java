c9847421073704649ce370cc34392a8d5b17709c
Hanbyul Lee <starrybleu@gmail.com>
2019-10-05 23:05:19 +0900

    예외 처리 보강

--- src/main/java/io/github/starrybleu/sideproject0/api/ExceptionHandlers.java
+++ src/main/java/io/github/starrybleu/sideproject0/api/ExceptionHandlers.java
@@ -1,8 +1,12 @@
 package io.github.starrybleu.sideproject0.api;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,11 +14,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionHandlers {
 
-    @ExceptionHandler(Throwable.class)
-    public ResponseEntity<Throwable> handleUncaughtException(Throwable t) {
-        log.error("", t);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(t);
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorHttpResponse> handleUsernameNotFoundException(UsernameNotFoundException e) {
+        log.warn("", e);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorHttpResponse(e));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorHttpResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.warn("", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorHttpResponse(e));
+    }
+
+    @Data
+    static class ErrorHttpResponse {
+        String error;
+        String message;
+
+        ErrorHttpResponse(Throwable t) {
+            this.error = t.getClass().getSimpleName();
+            this.message = t.getMessage();
+        }
     }
 
 
