ece45411183311d0ed346a2d30a0cbb8c5dfada5
Hanbyul Lee <starrybleu@gmail.com>
2019-10-05 14:50:11 +0900

    Initial commit

---
+++ src/main/java/io/github/starrybleu/sideprject0/TaxiAllocatingApplication.java
@@ -0,0 +1,13 @@
+package io.github.starrybleu.sideprject0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiAllocatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiAllocatingApplication.class, args);
+    }
+
+}
